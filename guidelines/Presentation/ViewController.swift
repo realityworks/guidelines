2a01be5737ff7e604ba864bfd6fa745a7b514533
Peter Suwara <psuwara@realityworks.com.au>
2020-05-12 11:41:25 +0300

    Initial project

---
+++ guidelines/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guidelines
+//
+//  Created by Piotr Suwara on 12/5/20.
+//  Copyright © 2020 Realityworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
